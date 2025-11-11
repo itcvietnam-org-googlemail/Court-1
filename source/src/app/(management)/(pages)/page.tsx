ed47906f26336b99ef8edede47eb4e54d9a67152
itcvietnam-org-googlemail <itcvietnam.org@googlemail.com>
2025-11-11 11:33:04 +0000

    SDK

--- source/src/app/(management)/(pages)/page.tsx
+++ source/src/app/(management)/(pages)/page.tsx
@@ -1,33 +1,111 @@
-import { createDirectus, rest, readItems } from '@directus/sdk';
+import { createDirectus, rest, readItems, createItem , Query } from '@directus/sdk';
+import { JSX } from 'react';
+
+interface Category {
+  id: string;
+  name: string;
+  status: string;
+  articles: ArticleCategory[];
+}
 
 interface Article {
   id: string;
   title: string;
   status: string;
+  categories: ArticleCategory[];
 }
 
-interface Articles {
+interface ArticleCategory {
+  id: number;
+  articles_id: Article;
+  categories_id: Category;
+}
+
+interface ManySchema {
   articles: Article[];
+  categories: Category[];
+  articles_categories: ArticleCategory[];
 }
 
 export default async function Page() {
   const apiUrl = 'https://cuddly-trout-4jv547pr97v43qgx-8055.app.github.dev';
 
-  const client = createDirectus<Articles>(apiUrl).with(rest());
+  const query: Query<ManySchema, Article> = {
+    limit: 20,
+    offset: 0,
+  };
+
+  const client = createDirectus<ManySchema>(apiUrl).with(rest());
   const articles = await client.request(readItems('articles', {
     filter: {
       status: {
-        _in: ['published', 'draft']
+        _in: ['draft', 'published']
+      },
+      categories: {
+        categories_id: {
+          status: {
+            _in: ['published']
+          }
+        }
       }
     },
-    limit: 2
+    //search: 'art',
+    fields: [
+      '*',
+      {
+        categories: [
+          {
+            categories_id: ['*']
+          }
+        ]
+      }
+    ],
+    sort: ['-title'],
+    deep: {
+      categories: {
+        _limit: 10
+      }
+    }
   }));
+
+  console.log(articles);
+
+  ///////
+  //const client = createDirectus(apiUrl).with(rest());
+  /*
+  const result = await client.request(createItem('articles', {
+    title: 'Article 4.2',
+    status: 'draft',
+    categories: [
+      {
+        categories_id: {
+          id: '9a8fbfd3-eb9e-436c-961c-b249468ee926'
+        }
+      }
+    ]
+  }));
+  */
+  ///////
 
   return (<div>
     <ul>
-      {articles.map((article: Article) => (
-        <li key={article.id}>{article.title} ({article.status})</li>
+      {articles.map((article) => (
+        <li key={article.id}>
+          {article.title}
+          ({article.status})
+          ({article.categories_count})
+          |
+          {(article.categories) ? article.categories[0].categories_id.name : ''}
+          ({(article.categories) ? article.categories[0].categories_id.status : ''})
+          |
+          {article.categories?.map((category) => (
+            <span key={category.categories_id.id} style={{paddingRight: '10px'}}>
+              {category.categories_id.name}
+            </span>
+          ))}
+        </li>
       ))}
     </ul>
   </div>);
+  
 }
